8f51b6205fdcfb1d91598526cc7a804c67953c4c
Shoeb Mohammad <shoeb.mo87@gmail.com>
2022-04-05 03:00:38 +0600

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        Crypto Yoyo Club is Powered By <a href="https://www.animationbd.com/">AnimationBD</a>
+        Crypto Yoyo Club is Powered By <a href="https://www.animationbd.com/"> AnimationBD </a>
       </footer>
     </>
   )
